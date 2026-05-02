c617a62c1b9574674396cb0d98a21db99c7d77b0
Kaltham Alhashmi <s202371470@kfupm.edu.sa>
2026-05-02 14:51:22 +0300

    Update sonner.jsx

--- src/components/ui/sonner.jsx
+++ src/components/ui/sonner.jsx
@@ -1,5 +1,19 @@
 import { Toaster as Sonner } from "sonner";
 
-const Toaster = () => <Sonner position="top-right" richColors />;
+const Toaster = () => (
+  <>
+    <style>{`
+      [data-sonner-toast] > [data-close-button] {
+        left: auto !important;
+        right: 0 !important;
+        transform: translate(35%, -35%) !important;
+      }
+      [data-sonner-toast] [data-content] {
+        padding-right: 1.25rem;
+      }
+    `}</style>
+    <Sonner position="top-right" richColors closeButton />
+  </>
+);
 
 export { Toaster };
